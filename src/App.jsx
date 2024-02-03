e566d1e636158fcd67646905bb3b96ad89ea6b12
Menfi2904 <menfigarcia31@gmail.com>
2024-02-03 17:25:50 -0600

    secondCommit

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,36 @@ const ENDPOINT_API = "https://rickandmortyapi.com/api/character";
 function App() {
   const [characters, setCharacter] = useState([]);
   const [search, setSearch] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     if (search) {
       fetch(`${ENDPOINT_API}?name=${search}`)
         .then((res) => res.json())
-        .then((data) => setCharacter(data.results));
+        .then((data) => {
+          if (data.results.length === 0) {
+            setError(`No se encontro el personaje ${search}`);
+          } else {
+            setError(null);
+            setCharacter(data.results);
+          }
+        })
+        .catch((error) => {
+          setError(`Hubor un error en la busqueda: ${error.message}`);
+        });
     }
   }, [search]);
 
   const handleSearch = (searchTerms) => {
     setSearch(searchTerms);
+    setError(null);
   };
 
   return (
     <div>
       <Search onSearch={handleSearch} />
       <div>
+        {error}
         {characters.map((character) => (
           <div key={character.id}>
             <div>{character.name}</div>
@@ -31,7 +44,7 @@ function App() {
         ))}
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
